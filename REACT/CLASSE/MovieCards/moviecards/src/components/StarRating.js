a57511dc9b5db7f256c224f7a49719d82c188eba
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-01-15 09:07:28 +0100

    Example MovieCards REACT

--- REACT/CLASSE/MovieCards/moviecards/src/components/StarRating.js
+++ REACT/CLASSE/MovieCards/moviecards/src/components/StarRating.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 
 import './StarRating.css';
 
+const width = 110;
+
+const styles = {
+    starsInner: {
+        width: `${width}px`
+    },
+    starsEmptyInner: {
+        position: 'absolute',
+        width: `${width}px`
+    },
+    starsOuter: {
+        overflow: 'hidden'
+    },
+    star: {
+        padding: '1px'
+    }
+};
+
+const cropWidth = (rating) => {
+    return Math.floor(rating * width / 5);
+};
 
 
 class StarRating extends Component {
@@ -12,23 +33,26 @@ class StarRating extends Component {
     }
 
 	render() {
+
+		const containerStyle = { width: `${cropWidth(this.props.rating)}px` };
+
 	    return (
 	    	<div>
-	    	    <div>
-	    	        <div>
-	    	            <div>
-	    	                <i className="fa fa-star-o fa-lg"></i>
-	    	                <i className="fa fa-star-o fa-lg"></i>
-	    	                <i className="fa fa-star-o fa-lg"></i>
-	    	                <i className="fa fa-star-o fa-lg"></i>
-	    	                <i className="fa fa-star-o fa-lg"></i>
+	    	    <div style={styles.starsOuter}>
+	    	        <div style={containerStyle}>
+	    	            <div style={styles.starsEmptyInner}>
+	    	                <i className="fa fa-star-o fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star-o fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star-o fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star-o fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star-o fa-lg" style={styles.star}></i>
 	    	            </div>
-	    	            <div>
-	    	                <i className="fa fa-star fa-lg"></i>
-	    	                <i className="fa fa-star fa-lg"></i>
-	    	                <i className="fa fa-star fa-lg"></i>
-	    	                <i className="fa fa-star fa-lg"></i>
-	    	                <i className="fa fa-star fa-lg"></i>
+	    	            <div style={styles.starsInner}>
+	    	                <i className="fa fa-star fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star fa-lg" style={styles.star}></i>
+	    	                <i className="fa fa-star fa-lg" style={styles.star}></i>
 	    	            </div>
 	    	        </div>
 	    	    </div>
