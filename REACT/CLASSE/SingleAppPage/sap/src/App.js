c15a0988f7f1569fa1bdac2321de4c2827943b87
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-01-18 11:01:04 +0100

    Single Page Application REACT

---
+++ REACT/CLASSE/SingleAppPage/sap/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {Route, NavLink, HashRouter} from "react-router-dom";
+
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+      <div className="App">
+        <h1>Simple Single Page App (SPA)</h1>
+            <ul className="header">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/stuff">Stuff</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+        <div className="content">
+          <Route exact path="/" component={Home}/>
+          <Route path="/stuff" component={Stuff}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
