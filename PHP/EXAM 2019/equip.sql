47dfe5d4b570e7944623587cb5e83d028efa6ec1
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-03-05 18:57:08 +0100

    Database Example PHP Exam

---
+++ PHP/EXAM 2019/equip.sql
@@ -0,0 +1,49 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 05-03-2019 a las 17:40:40
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `futbol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equip`
+--
+
+CREATE TABLE IF NOT EXISTS `equip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(20) NOT NULL,
+  `posicio` int(5) NOT NULL,
+  `punts` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `equip`
+--
+
+INSERT INTO `equip` (`id`, `nom`, `posicio`, `punts`) VALUES
+(1, 'CD Manacor', 1, 45),
+(2, 'CE Cardassar', 2, 41),
+(3, 'RCD Mallorca', 4, 38),
+(4, 'Poblense', 3, 39);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
