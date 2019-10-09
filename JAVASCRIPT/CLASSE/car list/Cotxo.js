a54e32d2bb5551f78b821d837d22bf773d916f4e
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-10-09 11:11:31 +0200

    car list

---
+++ JAVASCRIPT/CLASSE/car list/Cotxo.js
@@ -0,0 +1,38 @@
+class Car {
+	
+	constructor(matricula, marca, color, preu){
+		this.matricula=matricula;
+		this.marca=marca;
+		this.color=color;
+		this.preu=preu;
+		this.divisa = "€";
+	}
+
+	info(){
+		return this.matricula+", "+this.marca+", "+this.color+", "+this.preu+this.divisa;
+	}
+
+	info(){
+		return this.matricula+", "+this.marca+", <i class='fa fa-car' style='color:"+this.color+"'></i>, "+this.preu+this.divisa;
+	}
+
+	color(){
+		return this.color;
+	}
+
+	convertToDolars(){
+		if(this.divisa=="€"){
+			this.preu = Math.round(this.preu * 1.09,2);
+			this.divisa ="$";
+		}
+	}
+
+	convertToEuros(){
+		if(this.divisa=="$"){
+			this.preu = Math.round(this.preu / 1.09, 2);
+			this.divisa ="€";
+		}
+	}
+
+
+}
