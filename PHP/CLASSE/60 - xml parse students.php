313d163cec6747395978fb7cf6b6fbc391653be7
profeweb <42413333+mitjanit@users.noreply.github.com>
2020-02-25 15:27:05 +0100

    XML Parse CLassroom.XML

---
+++ PHP/CLASSE/60 - xml parse students.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>students</h1>
+<?php
+//Initialize the XML parser
+$parser=xml_parser_create();
+
+//Function to use at the start of an element
+function start($parser,$element_name,$element_attrs){
+
+  }
+
+//Function to use at the end of an element
+function stop($parser,$element_name){
+  switch($element_name){
+
+  }
+
+//Function to use when finding character data
+function char($parser,$data){
+  
+  }
+
+//Specify element handler
+xml_set_element_handler($parser,"start","stop");
+
+//Specify data handler
+xml_set_character_data_handler($parser,"char");
+
+//Open XML file
+$fp=fopen("classroom.xml","r");
+
+//Read data
+while ($data=fread($fp,4096)){
+  xml_parse($parser,$data,feof($fp)) or
+  die (sprintf("XML Error: %s at line %d",
+  xml_error_string(xml_get_error_code($parser)),
+  xml_get_current_line_number($parser)));
+  }
+
+//Free the XML parser
+xml_parser_free($parser);
+
+?>
+</body>
+</html>
