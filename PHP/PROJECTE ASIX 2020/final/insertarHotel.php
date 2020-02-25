aaf470638774035a3b7d998774d6938e71a4db55
profeweb <42413333+mitjanit@users.noreply.github.com>
2020-02-25 16:19:45 +0100

    Afegit Bootstrap al Formulari

--- PHP/PROJECTE ASIX 2020/final/insertarHotel.php
+++ PHP/PROJECTE ASIX 2020/final/insertarHotel.php
@@ -1,40 +1,90 @@
 <?php
 	include("cap.php");
 ?>
-	<h1>FORM INSERT HOTEL.</h1>
+<div class="container">
+	<h1><i class='fa fa-hotel' aria-hidden='true'></i> Nou Hotel.</h1>
+	<hr/>
 	<form action="insertHotel.php" method="post">
-			NOM:<input type="text" name="nom" /><br/>
-			DIR:<input type="text" name="adreca" /><br/>
-			POBLE:<input type="text" name="poble" /><br/>
-			TELEFON:<input type="text" name="telefon" /><br/>
-			FAX:<input type="text" name="fax" /><br/>
-			WEB:<input type="text" name="web" /><br/>
-			LAT / LONG:<input type="text" name="lat" size="5" /><input type="text" name="long" size="5" /><br/>
-			ID_PROVINCIA:<select  name="provincia">
-		<?php 
-		require('connexio.php');
-		$sql2 = "SELECT * FROM provincia ORDER BY nom";
-		$rs2 = mysqli_query($con, $sql2) or die();
-		while($row2 = mysqli_fetch_array($rs2)){
-			echo "<option value='".$row2['id']."' >";
-			echo $row2['nom'];
-			echo "</option>";
-		}
-		?>
-		</select><br/>
-		ID_PAIS: <select  name="pais">
-		<?php 
-		$sql2 = "SELECT * FROM pais ORDER BY nom";
-		$rs2 = mysqli_query($con, $sql2) or die();
-		while($row2 = mysqli_fetch_array($rs2)){
-			echo "<option value='".$row2['id']."' >";
-			echo $row2['nom'];
-			echo "</option>";
-		}
-		?>
-		</select><br/>
-			FOTO:<input type="file" name="foto" /><br/>
-			<input type="submit" value="INSERT"/>
+		<div class="row">
+			<div class="col form-group">
+	    		<label for="nom">Nom:</label>
+				<input type="text" name="nom"  class="form-control" />
+			</div>
+			<div class=" col form-group">
+				<label for="adreca">Adreça:</label>
+				<input type="text" name="adreca"  class="form-control" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col form-group">
+	    		<label for="poble">Poble:</label>
+	    		<input type="text" name="poble"  class="form-control" />
+	    	</div>
+	    	<div class="col form-group">
+	    		<label for="telefon">Telefon:</label>
+				<input type="text" name="telefon"  class="form-control" />
+			</div>
+			<div class="col form-group">
+	    		<label for="fax">Fax:</label>
+				<input type="text" name="fax"  class="form-control" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col form-group">
+	    		<label for="web">Web:</label>
+				<input type="text" name="web"  class="form-control" />
+			</div>
+			<div class="col form-group">
+	    		<label for="lat">Latitud:</label>
+	    		<input type="text" name="lat" size="5"   class="form-control" />
+			</div>
+			<div class="col form-group">
+	    		<label for="long">Longitud:</label>
+	    		<input type="text" name="long" size="5"  class="form-control" />
+	    	</div>
+	    </div>
+	    <div class="row">
+			<div class="col form-group">
+	    		<label for="web">Provincia:</label>
+				<select  name="provincia"  class="form-control">
+					<?php 
+					require('connexio.php');
+					$sql2 = "SELECT * FROM provincia ORDER BY nom";
+					$rs2 = mysqli_query($con, $sql2) or die();
+					while($row2 = mysqli_fetch_array($rs2)){
+						echo "<option value='".$row2['id']."' >";
+						echo $row2['nom'];
+						echo "</option>";
+					}
+					?>
+					</select>
+			</div>
+			<div class="col form-group">
+	    		<label for="web">Pais:</label>
+				<select  name="pais"  class="form-control">
+				<?php 
+				$sql2 = "SELECT * FROM pais ORDER BY nom";
+				$rs2 = mysqli_query($con, $sql2) or die();
+				while($row2 = mysqli_fetch_array($rs2)){
+					echo "<option value='".$row2['id']."' >";
+					echo $row2['nom'];
+					echo "</option>";
+				}
+				?>
+				</select>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col form-group">
+	    		<label for="foto">Foto:</label>
+				<input type="file" name="foto"  class="form-control" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+			<input type="submit" class="btn btn-primary" value="Inserir"/>
+			</div>
+		</div>
 	</form>
 <?php
 	include("footer.php");
--- PHP/PROJECTE ASIX 2020/final/taulaHotel.php
+++ PHP/PROJECTE ASIX 2020/final/taulaHotel.php
@@ -1,16 +1,18 @@
 <?php
 include("cap.php");
 ?>
-	<h1>Welcome SELECT.</h1>
+<div class="container">
+	<h1><i class='fa fa-hotel' aria-hidden='true'></i> Hotels</h1>
+	<hr/>
 	<a href="insertarHotel.php"><button type="button" class="btn btn-primary"><i class="fa fa-plus" aria-hidden="true"></i> Nou Hotel</button></a><br/><br/>
 	<table id='dt' class='table table-striped table-bordered' style='width:100%' >
 		<thead>
-			<tr><th>Hotel</th><th>Poble</th><th>Web</th><th>Telefon</th><th>Editar</th><th>Borrar</th></tr>
+			<tr><th>Hotel</th><th>Poble</th><th>Web</th><th>Telefon</th><th>Fax</th><th>Editar</th><th>Borrar</th></tr>
 		</thead>
 		<tbody>
 	<?php
 		require('connexio.php');
-		$sql = "SELECT * FROM hotel ORDER BY nom";
+		$sql = "SELECT * FROM hotel ORDER BY nom ASC";
 		$rs = mysqli_query($con, $sql) or die();
 			while($row = mysqli_fetch_array($rs)){
 				echo "<tr>";
@@ -18,6 +20,7 @@ include("cap.php");
 					echo "<td>".$row['poblacio']."</td>";
 					echo "<td>".$row['web']."</td>";
 					echo "<td>".$row['telefon']."</td>";
+					echo "<td>".$row['fax']."</td>";
 					echo "<td><a href='editarHotel.php?id=".$row['id']."'><button type='button' class='btn btn-warning'><i class='fa fa-pencil' aria-hidden='true'></i> Editar</button></a></td>";
 					echo "<td><a href='deleteHotel.php?id=".$row['id']."'><button type='button' class='btn btn-danger'><i class='fa fa-trash' aria-hidden='true'></i> Borrar</button></a></td>";
 				echo "</tr>";
@@ -26,7 +29,7 @@ include("cap.php");
 		?>
 		</tbody>
 	</table>
-
+</div>
 	<script>
 		
 		$(document).ready( function () {
