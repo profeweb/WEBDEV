6ec7891693dfbd8775a4aaf29ad2d59e37c25d64
profeweb <42413333+mitjanit@users.noreply.github.com>
2020-11-26 09:09:43 +0100

    hint Marques

---
+++ AJAX/CLASSE/concessionari/marquesHint.php
@@ -0,0 +1,33 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "concesionari");
+$stmt = $conn->prepare("SELECT * FROM marca ORDER BY nom");
+$stmt->execute();
+$result = $stmt->get_result();
+
+$a = array();
+while($row = mysqli_fetch_array($result)) {
+	array_push($a, $row['nom']);
+}
+
+$a = array_column($result, 'nom');
+
+$q = $_REQUEST["q"];
+$hint = "";
+
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+echo $hint === "" ? "no suggestion" : $hint;
+?>
